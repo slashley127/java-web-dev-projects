dec4fd58ea0063d5ee96f84fadf941542873d945
Slash <slashley127@gmail.com>
2023-08-16 17:45:02 -0500

    added Area class and created Circle class

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,11 @@
+package org.launchcode;
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a radius: ");
+
+    }
+}
