17903dc9e8f062af611c4e3e27eefd5baec8441d
Slash <slashley127@gmail.com>
2023-08-16 18:02:54 -0500

    datatypes studio complete

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -4,8 +4,17 @@ import java.util.Scanner;
 public class Area {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-
         System.out.println("Enter a radius: ");
+        double radius = input.nextDouble();
+
+        double areaOfCircle = Circle.getArea(radius);
+
+        System.out.println("The area of the circle is " + areaOfCircle);
+
+
+
+
+
 
     }
 }
